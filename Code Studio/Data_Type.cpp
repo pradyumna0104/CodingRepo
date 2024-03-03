9e02bafd9db07d11a3dd35ae4c3a1b59db35e668
Pradyumna Sahu <155447510+pradyumna0104@users.noreply.github.com>
2024-03-03 18:26:22 +0530

    Add files via upload

---
+++ Code Studio/Data_Type.cpp
@@ -0,0 +1,10 @@
+int dataTypes(string type) {
+	// Write your code here
+	if(type == "Integer")  return 4;
+	else if(type == "Long")  return 8;	
+	else if(type == "Float")  return 4;
+	else if(type == "Double")  return 8;
+	else if(type == "Character")  return 1;
+	else return -1;
+
+}
