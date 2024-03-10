c0c9fddf07acf9a0936dacfcaf2de7b34f243e8a
Pradyumna Sahu <155447510+pradyumna0104@users.noreply.github.com>
2024-03-10 20:27:49 +0530

    Add files via upload

---
+++ Code Studio/Find_power_of_a_number.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+    int n,m;
+    cin>>n>>m;
+    int ans = pow(n,m);
+    cout<<ans;
+
+    return 0;
+}
