25eecaed23ffbcaf6530a7d3bf78c45503944f67
Pradyumna Sahu <155447510+pradyumna0104@users.noreply.github.com>
2024-03-11 20:47:51 +0530

    Add files via upload

---
+++ leet code/mar-11(26).cpp
@@ -0,0 +1,15 @@
+//26. Remove Duplicates from Sorted Array
+#include <bits/stdc++.h>
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j = 1;
+        for(int i =1;i<nums.size();i++){
+            if(nums[i] != nums[i-1]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+};
