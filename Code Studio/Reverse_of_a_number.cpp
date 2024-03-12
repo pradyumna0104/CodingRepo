88842fc5bdcdb6fc648381c12f09511ce5c36ef3
Pradyumna Sahu <155447510+pradyumna0104@users.noreply.github.com>
2024-03-12 22:20:22 +0530

    Add files via upload

---
+++ Code Studio/Reverse_of_a_number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+	int n;
+	cin>>n;
+	int reverse = 0;
+	while(n>0){
+		int rem = n%10;
+		reverse = reverse*10 + rem;
+		n/=10;
+	}
+	cout<<reverse;
+}
