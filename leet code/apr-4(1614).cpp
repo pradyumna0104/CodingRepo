fa15832dba3f9d042e40a42eaae8682a971e612f
Pradyumna Sahu <155447510+pradyumna0104@users.noreply.github.com>
2024-04-07 17:16:02 +0530

    Add files via upload

---
+++ leet code/apr-4(1614).cpp
@@ -0,0 +1,18 @@
+//1614. Maximum Nesting Depth of the Parentheses
+class Solution {
+public:
+    int maxDepth(string s) {
+        int count = 0;
+        int max_num = 0;
+        for (char c : s) {
+            if (c == '(') {
+                count++;
+                if (max_num < count)
+                    max_num = count;
+            } else if (c == ')') {
+                count--;
+            }
+        }
+        return max_num;
+    }
+};
